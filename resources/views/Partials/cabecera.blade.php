42ac4ae6fbd7b73c6e1520712b15af2ef4898236
pau <pau_ll_m@hotmail.com>
2020-04-13 17:35:00 +0200

    Insetar el login de laravel en mi diseño web

--- resources/views/Partials/cabecera.blade.php
+++ resources/views/Partials/cabecera.blade.php
@@ -12,14 +12,72 @@
         <div class="col-md-3 col-8 my-5 my-md-4 text-center">
             <div class="btn-group mr-5 mr-md-4">
                 <a href="#" data-toggle="dropdown"><img class="svgTamanyo" class="mt-4" src="{{asset('img/user.svg')}}" alt="Usuario"></a>
+                @if(!Auth::user())
+                <div class="container dropdown-menu dropdown-menu-right">
+                    <div class="row justify-content-center">
+                        <div class="col-md-12">
+                            <form class="px-4 py-3" method="POST" action="{{ route('login') }}">
+                                @csrf
+                                <div class="form-group">
+                                    <label for="email" class="col-form-label text-md-right">{{ __('Correo Electronico') }}</label>
+                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                    @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+                                <div class="form-group">
+                                    <label for="password" class="col-form-label text-md-right">{{ __('Contraseña') }}</label>
+                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                    @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                    <label class="form-check-label" for="remember">
+                                        {{ __('Recordar Datos') }}
+                                    </label>
+                                </div>
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Entrar') }}
+                                </button>
+                            </form>
+                            <div class="dropdown-divider"></div>
+                            @if (Route::has('password.request'))
+                            <a class="btn btn-link" href="{{ route('password.request') }}">
+                                {{ __('¿Has olvidado tu contraseña?') }}
+                            </a>
+                            <a class="btn btn-link" href="{{ route('register') }}">
+                                {{ __('¿Nuevo en Kangoo Home? Registrarte aquí!') }}
+                            </a>
+                            @endif
+                        </div>
+                    </div>
+                </div>
+                @else
                 <div class="dropdown-menu mr-5">
                     <a class="dropdown-item" href="#">Action</a>
                     <a class="dropdown-item" href="#">Another action</a>
                     <a class="dropdown-item" href="#">Something else here</a>
                     <div class="dropdown-divider"></div>
                     <a class="dropdown-item" href="#">Separated link</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="btn btn-link" href="{{ route('logout') }}"
+                       onclick="event.preventDefault();
+                                document.getElementById('logout-form').submit();">
+                        {{ __('Cerrar Session') }}
+                    </a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
                 </div>
+                @endif
             </div>
+            @if(Auth::user())
             <div class="btn-group mr-5 mr-md-4">
                 <a href="#" data-toggle="dropdown"><img class="svgTamanyo" class="mt-4" src="{{asset('img/notificacion.svg')}}" alt="Correo"></a>
                 <div class="dropdown-menu mr-5">
@@ -30,6 +88,7 @@
                     <a class="dropdown-item" href="#">Separated link</a>
                 </div>
             </div>
+            @endif
             <img class="svgTamanyo" class="mt-4" src="{{asset('img/corazon.svg')}}" alt="Me gusta">
         </div>
     </div>
